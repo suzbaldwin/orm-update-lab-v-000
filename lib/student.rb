26e75d83a30aba4ddc1cd3c42d886b9ee45be0a5
suzy baldwin <gbaldwin24@comcast.net>
2018-02-13 21:09:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
     sql = "SELECT name FROM students WHERE name = ?"
     row = DB[:conn].execute(sql, name)
 
-    student = Student.new_from_db(row[0], row[1], row[2])
+    student = Student.new_from_db(row[1])
 
   end
 
